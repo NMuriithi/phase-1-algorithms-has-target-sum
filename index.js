bb82ec01fb228fce30b1c8367d48d57bc4cd5562
Nicholus <nicholus.muriithi@student.moringaschool.com>
2023-11-10 10:54:39 -0800

    Second algorithm

--- index.js
+++ index.js
@@ -19,6 +19,17 @@ if (combinations.includes(target)) {
 */
 
 /* 
+function hasTargetSum(array, target) {
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] + array[j] === target) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
   Add your pseudocode here
 */
 
